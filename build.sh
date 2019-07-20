1979441e1429211295593465135e061ba3e058cd
Egor Sharapov <egor.sharapov@gmail.com>
2019-07-20 14:14:03 +0300

    builder lil fix

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ npm run build
 npm run export
 touch out/.nojekyll
 cat CNAME > out/CNAME
+git add out
 git commit out -m "bump build"
 git push origin master
 git subtree push --prefix out origin gh-pages
