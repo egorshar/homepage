f58bfb03e4287391a697b8e88fcdb93d1db567b1
Egor Sharapov <egor.sharapov@gmail.com>
2019-07-21 13:54:58 +0300

    bump build

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8c168e9bfa5258c813bd";
+/******/ 	var hotCurrentHash = "202e1b0634cc74026880";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
