8d483016411021793c77dc92ab5ce6fbe48bee41
egorshar <egor.sharapov@gmail.com>
2022-03-22 08:24:55 +0300

    device width fix

--- pages/_app.js
+++ pages/_app.js
@@ -19,10 +19,7 @@ class EgorShar extends App {
       <>
         <Head>
           <title>Egor Sharapov's Homepage</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
+          <meta name="viewport" content="width=device-width" />
 
           {!isDev() && (
             <>
