b6eeab48cb7ae4487fe77c49568ae32e6d663489
Egor Sharapov <egor.sharapov@gmail.com>
2025-11-02 11:10:07 +0500

    Shareware domain fix again

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import { headers } from 'next/headers';
 import Index from '@/components/Index/Index';
-import IndexShareware from '@/components/IndexShareware/IndexShareware';
 import { getDictionary } from '@/utils/dictionaries';
 import { isSharewareDomain } from '@/utils/index';
+import ConsultingMentoring from '@/components/ConsultingMentoring/ConsultingMentoring';
 
 export default async function Page({ params: { lang } }) {
   const requestHeaders = headers();
   const host = requestHeaders.get('host') || '';
-  const t = await getDictionary(isSharewareDomain(host) ? 'ru' : lang);
-  const IndexToRender = isSharewareDomain(host) ? IndexShareware : Index;
+
+  if (isSharewareDomain(host)) {
+    return (
+      <div className="max-w-7xl pb-40">
+        <ConsultingMentoring />
+      </div>
+    )
+  }
+
+  const t = await getDictionary(lang);
 
   return (
     <div className="max-w-7xl pb-40">
-      <IndexToRender t={t} locale={lang} />
+      <Index t={t} locale={lang} />
     </div>
   );
 }
