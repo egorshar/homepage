613c9a7ee9da698cf4cb8a1bae99145c13eeccfc
Egor Sharapov <egor.sharapov@gmail.com>
2025-11-02 08:52:53 +0500

    egorshareware homepage

--- app/areware/page.tsx
+++ app/areware/page.tsx
@@ -1,11 +1,11 @@
 import IndexShareware from '@/components/IndexShareware/IndexShareware';
-import { getDictionary } from './dictionaries';
+import { getDictionary } from '@/utils/dictionaries';
 
-export default async function Page({ params: { lang } }) {
-  const t = await getDictionary(lang);
+export default async function Page() {
+  const t = await getDictionary('ru');
   return (
-    <div className="max-w-7xl pb-40">
-      <IndexShareware t={t} locale={lang} />
+    <div className='max-w-7xl pb-40'>
+      <IndexShareware t={t} locale='ru' />
     </div>
   );
 }
--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -10,7 +10,7 @@ const Sphere = dynamic(() => import('./Sphere/Sphere'), {
   ssr: false,
   loading: () => (
     <h1
-      className='absolute right-0 mr-[1px] -mt-[3px] w-[50px] h-[50px] bg-black dark:bg-gray-800 rounded-full text-white flex pt-[3px] pl-[3px]'>sh</h1>
+      className='absolute left-full ml-3 -mt-[3px] w-[50px] h-[50px] bg-black dark:bg-gray-800 rounded-full text-white flex pt-[3px] pl-[3px]'>sh</h1>
   ),
 });
 
@@ -25,25 +25,27 @@ const LogoWithIndex = ({ isShareware = false }: { isShareware?: boolean }) => {
   }, [theme, setTheme]);
 
   return (
-    <div className='flex relative pr-[64px] select-none'>
-      <h1 className='mb-0'>
-        {isShareware ? (<Link href={egorShLink}>egor</Link>) : 'egor'}
-      </h1>
+    <div className='flex relative select-none'>
+      <div className="flex relative">
+        <h1 className='mb-0'>
+          {isShareware ? (<Link href={egorShLink}>egor</Link>) : 'egor'}
+        </h1>
 
-      <Sphere
-        className='absolute right-0 -mr-[24px] -mt-[53px] w-[100px]'
-        theme={theme === 'dark' ? 'dark' : 'light'}
-        toggleTheme={() => {
-          if (theme === 'dark') {
-            removeTheme();
-            document.documentElement.classList.remove('dark');
-          } else {
-            setTheme('dark');
-            document.documentElement.classList.add('dark');
-          }
-        }}
-      />
-      {isShareware && <h1 className='absolute mb-0 left-full ml-3'>areware</h1>}
+        <Sphere
+          className='absolute left-full -ml-[11px] -mt-[53px] w-[100px]'
+          theme={theme === 'dark' ? 'dark' : 'light'}
+          toggleTheme={() => {
+            if (theme === 'dark') {
+              removeTheme();
+              document.documentElement.classList.remove('dark');
+            } else {
+              setTheme('dark');
+              document.documentElement.classList.add('dark');
+            }
+          }}
+        />
+      </div>
+      {isShareware && <h1 className='mb-0 left-full ml-[4.8rem]'>areware</h1>}
     </div>
   );
 };
